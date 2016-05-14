f5210daec4f28178c791d1786b9b84affe498097
akplan <alyssa.plan@gmail.com>
2016-05-14 19:59:26 -0400

    uploading platform practicels!

---
+++ platform-practice.love/main.lua
@@ -0,0 +1,24 @@
+player = {}
+player.x = 0
+
+function love.load()
+    x = 0
+    y = 0
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        x = x + 1
+    elseif love.keyboard.isDown("left") then
+        x = x - 1
+    elseif love.keyboard.isDown("up") then
+        y = y - 1
+    elseif love.keyboard.isDown("down") then
+        y = y + 1
+    end
+end
+
+function love.draw()
+    love.graphics.rectangle("fill",x,y,100,100)
+    --x = x + 1
+end
